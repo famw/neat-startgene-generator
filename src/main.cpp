7b59ecef9aeddc696e1be508f80d0accd0ef443e
Martin Duarte <martinduartemore@gmail.com>
2016-11-16 00:52:01 -0200

    Add extra inputs capability

--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,33 @@
 
 int main(int argc, char **argv)
 {
-	if(argc < 3)
+	if(argc < 4)
 	{
-		std::cout << "Usage: ./generator [input_radius] [num_outputs]" << std::endl;
+		std::cout << "Usage: ./generator [vision_radius] [extra_inputs] [num_outputs]" << std::endl;
 		exit(0);
 	}
 
-	int input_radius = std::stoi(argv[1]);
-	int num_outputs = std::stoi(argv[2]);
+	int vision_radius = std::stoi(argv[1]);
+	int extra_inputs = std::stoi(argv[2]);
+	int num_outputs = std::stoi(argv[3]);
 
 	std::ofstream output_file("neat_startgenes");
 	output_file << "genomestart 1\n"; // start of file
 	output_file << "trait 1 0 0 0 0 0 0 0 0\n"; // not used but needed
 
 	// nodes
-	int num_inputs = (input_radius*2+1)*(input_radius*2+1);
-	for(int i=1; i<=num_inputs+1+num_outputs; i++)
+	int num_vision_inputs = (vision_radius*2+1)*(vision_radius*2+1);
+	for(int i=1; i<=num_vision_inputs+extra_inputs+1+num_outputs; i++)
 	{
-		if(i <= num_inputs) // input nodes
+		if(i <= num_vision_inputs) // input nodes
 		{
 			output_file << "node " << i << " 0 1 1\n";
 		}
-		else if(i == num_inputs+1) // bias node
+		else if(i > num_vision_inputs && i <= num_vision_inputs+extra_inputs)
+		{
+			output_file << "node " << i << " 0 1 1\n";
+		}
+		else if(i == num_vision_inputs+extra_inputs+1) // bias node
 		{
 			output_file << "node " << i << " 0 1 3\n";
 		}
@@ -37,11 +42,11 @@ int main(int argc, char **argv)
 
 	// genes
 	int innovation_num = 1;
-	for(int in=1; in<=num_inputs+1; in++)
+	for(int in=1; in<=num_vision_inputs+extra_inputs+1; in++)
 	{
 		for(int out=1; out<=num_outputs; out++)
 		{
-			int out_num = num_inputs+1+out;
+			int out_num = num_vision_inputs+extra_inputs+1+out;
 			output_file << "gene 1 " << in << " " << out_num << " 0.0 0 " << innovation_num << " 0 1\n";
 			innovation_num++;
 		}
